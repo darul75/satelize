e6d093d020ff2f386931cbe9a04c2c55623c9989
darul <darul75@gmail.com>
2013-11-03 11:36:20 +0100

    refactoring code, minimized it

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ var http = require('http');
  http://www.telize.com/geoip/46.19.37.108
 */
 
-exports.serviceHost = 'www.telize.com';
-exports.servicePath = '/geoip';
-exports.serviceJSONP = '?callback=getgeoip';
+var serviceHost = 'www.telize.com';
+var servicePath = '/geoip';
+var serviceJSONP = '?callback=getgeoip';
 
 exports.satelize = function(options, next) {
-    var path = (options.ip ? ('/'+options.ip) : '') + (options.JSONP ? this.serviceJSONP : '');
+    var path = (options.ip ? ('/'+options.ip) : '') + (options.JSONP ? serviceJSONP : '');
     var options = {
-      hostname: this.serviceHost,
-      path: this.servicePath + path,
+      hostname: serviceHost,
+      path: servicePath + path,
       method: 'GET',
       port: 80
     };
@@ -22,23 +22,11 @@ exports.satelize = function(options, next) {
     var req = http.request(options, function(res) {
       res.setEncoding('utf8');
       var output = '';
-
-      res.on('data', function (chunk) {
-          output += chunk;
-      });
-
-      res.on('end', function() {                      
-        return next(null, output);        
-      });
+      res.on('data', function (chunk) { output += chunk; });
+      res.on('end', function() { return next(null, output); });
     });
-
-    req.on('error', function(e) {
-      return next(e);
-    });
-      
-    req.setTimeout(1000, function() {
-      return next(new Error('timeout'));
-    });
+    req.on('error', function(e) { return next(e); });      
+    req.setTimeout(1000, function() { return next(new Error('timeout')); });
 
     req.end();
 };
--- test/main.js
+++ test/main.js
@@ -6,7 +6,7 @@ describe('service calls', function() {
     describe('with ip arguments', function() {
         it('return Netherlands nice country', function(done) {
         	satelize.satelize({JSONP: false, ip:'46.19.37.108'}, function(err, geoData) {        		
-    			if (err) next(err);
+    			if (err) console.log(err);
     			
     			var d = JSON.parse(geoData);
     			assert.equal(d.country, 'Netherlands');
